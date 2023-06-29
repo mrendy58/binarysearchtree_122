782ca703444fb8318d0cde89f7dc4beb640d8e89
mrendy58 <derendyyy58@gmail.com>
2023-06-29 19:32:32 +0700

    membuat void preorder

--- binarysearchtree_122/binarysearchtree_122.cpp
+++ binarysearchtree_122/binarysearchtree_122.cpp
@@ -85,6 +85,20 @@ public :
             inorder(ptr->rightchild);
         }
     }
+    void preorder(Node* ptr)
+    {
+        if (ROOT == NULL)
+        {
+            cout << "tree is empty" << endl;
+            return;
+        }
+        if (ptr != NULL)
+        {
+            cout << ptr->info << " ";
+            preorder(ptr->leftchild);
+            preorder(ptr->rightchild);
+        }
+    }
 };
 int main()
 {
