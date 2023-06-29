532225377502157139fb252f574121b19697c485
mrendy58 <derendyyy58@gmail.com>
2023-06-29 18:40:06 +0700

    mengalokasikan memory untuk node baru

--- binarysearchtree_122/binarysearchtree_122.cpp
+++ binarysearchtree_122/binarysearchtree_122.cpp
@@ -30,7 +30,7 @@ public :
 
     void insert(string element) // insert a node in the binary search tree
     {
-
+        Node* newNode = new Node(element, NULL, NULL); // Allocate memory to new node
     }
 };
 int main()
